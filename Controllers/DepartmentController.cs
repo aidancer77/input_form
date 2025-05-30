90b0f0b886b39da97bf7af87eb0cd1ada28c9644
aidancer77 <107675611+aidancer77@users.noreply.github.com>
2025-05-30 17:00:35 +0300

    Add files via upload

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,17 @@
+﻿using EmployeeApi.Data;
+using EmployeeApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class DepartmentController(ApplicationDbContext context) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Department>>> GetEmployees()
+    {
+        return await context.Departments.ToListAsync();
+    }
+}
---
+++ Controllers/InfoSystemController.cs
@@ -0,0 +1,17 @@
+﻿using EmployeeApi.Data;
+using EmployeeApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class InfoSystemController(ApplicationDbContext context) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<InfoSystem>>> GetEmployees()
+    {
+        return await context.InfoSystems.ToListAsync();
+    }
+}
